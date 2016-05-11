0af96143cb710257b9e688cb03008d07410ef48e
daisukeishida <berg7295karajan@gmail.com>
2016-05-11 08:56:40 +0000

    third c2-commit

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,6 @@
 class MessagesController < ApplicationController
+ before_action :set_message, only: [:edit, :update, :destroy]
+ 
   def index
     @message = Message.new
     # Messageを全て取得する。
@@ -11,9 +13,29 @@ class MessagesController < ApplicationController
        redirect_to root_path , notice: 'メッセージを保存しました'
   end
   
+  def edit
+  end
+
+  def update
+    if @message.update(message_params)
+      redirect_to root_path , notice: 'メッセージを編集しました'
+    else
+      render 'edit'
+    end
+  end
+  
+  def destroy
+    @message.destroy
+    redirect_to root_path , notice: 'メッセージを削除しました'
+  end
+  
   private
   def message_params
     params.require(:message).permit(:name , :age , :body)
+  end
+  
+  def set_message
+    @message = Message.find(params[:id])
   end
 
 end
